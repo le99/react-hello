a69508f07308fe355f190e927c5439a52578487a
le <le.florez602@uniandes.edu.co>
2023-09-25 15:03:29 -0500

    Material Angular very basic settup

--- angularClient/src/app/app.module.ts
+++ angularClient/src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/navbar/Navbar.js
+++ client/src/navbar/Navbar.js
@@ -108,7 +108,7 @@ export default function MenuAppBar({backPage}) {
             </MenuItem>
           )}
 
-          {auth.user.email && (
+          {auth.user && (
             <div>
               <IconButton
                 size="large"
